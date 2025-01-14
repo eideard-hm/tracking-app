e20d785a049f7a69ab002969b9315ee746bb9194
eideard-hm <edierhernandezmo@gmail.com>
2025-01-13 19:07:20 -0500

    Add .npmrc file and update dependencies to save exact versions; enhance main.ts with HTTP client support

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
 
-import { routes } from './app/app.routes';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(withFetch()),
   ],
 });
