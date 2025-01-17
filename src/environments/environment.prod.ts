b124193ae96dcc9c30a74ad2102def86a0572334
eideard-hm <edierhernandezmo@gmail.com>
2025-01-16 19:24:28 -0500

    Add Mapbox access token to production environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxAccessToken:
+    'pk.eyJ1IjoiZWlkZWFyZC1obSIsImEiOiJjbTV6aDUyZW8wMmt3MmpvdWJsZWFiZWhvIn0.I1-67Rqe_cRypJke9TiKtQ',
 };
